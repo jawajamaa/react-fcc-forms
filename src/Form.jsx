82dc94b4b02daf5e0fc5643ef19425a029e696e0
Timothy Ryon <tim@timryon.com>
2024-03-09 14:26:27 -0500

    working validate login

--- src/Form.jsx
+++ src/Form.jsx
@@ -20,7 +20,43 @@ import { GrMail } from 'react-icons/gr'
 import { BsFillCheckSquareFill } from 'react-icons/bs'
 
 export const Form = () => {
+  const methods = useForm();
+
+  const onSubmit = methods.handleSubmit(data => {
+    console.log(data)
+  })
+
   return (
-    <div className="text-center mt-5">Welcome to React Forms Tutorial!</div>
+    <FormProvider {...methods}>
+        <form
+          onSubmit = { e => e.preventDefault()}
+          noValidate
+          className = "container"
+        >
+          <div className = "grid gap-5 md:grid-cols-2">
+            <Input
+              label = "name"
+              type = "text"
+              id = "name"
+              placeholder = "type your name..."
+            />  
+            <Input
+              label = "password"
+              type = "password"
+              id = "password"
+              placeholder = "type your password"
+            />
+          </div>
+          <div className="mt-5">
+            <button
+              onClick = {onSubmit}
+              className = " flex items-center gap-1 p-5 font-semibold text-white bg-blue-600 rounded-md hover:bg-blue-800"
+            >
+              <GrMail />
+              Submit Form
+            </button>        
+          </div>
+    </  form>
+    </FormProvider>
   )
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -12,9 +12,31 @@ import { useFormContext } from 'react-hook-form'
 import { AnimatePresence, motion } from 'framer-motion'
 import { MdError } from 'react-icons/md'
 
-export const Input = ({}) => {
-  return <div>input!</div>
-}
+export const Input = ({ label, type, id, placeholder }) => {
+  const { register } = useFormContext();
+
+  return (
+    <div className= "flex flex-col w-full gap-2">
+      <div className = "flex justify-between">
+        <label htmlFor = {id} className = "font-semibold capitalize">
+          {label}
+        </label>
+      </div>
+      <input
+        id = { id }
+        type = { type }
+        className = "w-full p-5 font-medium border rounded-md border-slate-300 placeholder:opacity-60"
+        placeholder = { placeholder }
+        {...register(label, {
+          required: {
+            value: true,
+            message: "required",
+          },
+        })}
+        />
+    </div>
+    )
+  }
 
 const InputError = () => {
   return <div>error</div>
